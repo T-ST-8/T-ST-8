83c048c8105b96e7e534cae56f21cd3a04c45550
Emma <empe0005@stud.kea.dk>
2024-09-17 11:17:41 +0200

    js start

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-fetch("https://axcgppzsihqhelmllyrd.supabase.co/rest/v1/T&SL", {
+fetch("https://axcgppzsihqhelmllyrd.supabase.co/rest/v1/T&SL?limit=6", {
   method: "GET",
   headers: {
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4Y2dwcHpzaWhxaGVsbWxseXJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NTg3NDgsImV4cCI6MjA0MTUzNDc0OH0.ZRk78YiB5wffWcrQYr-CRsh2yrb7ISHSGx4V7rQuO_8",
   },
 })
   .then((res) => res.json())
-  .then(console.log);
+  .then(showCategories);
 
 function showCategories(cats) {
   cats.forEach(showCategory);
@@ -19,7 +19,7 @@ function showCategory(cat) {
   clone.querySelector("h3").textContent = cat.type;
   clone.querySelector("a").href = `produktliste.html?type=${cat.type}`;
 
-  document.querySelector(".kategorier_grid").appendChild(clone);
+  document.querySelector(".kategori_grid").appendChild(clone);
 }
 
 /* function showData(items) {
--- produktliste.js
+++ produktliste.js
@@ -1,7 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const category = urlParams.get("category");
 
-fetch("https://axcgppzsihqhelmllyrd.supabase.co/rest/v1/T&SL", {
+fetch("https://axcgppzsihqhelmllyrd.supabase.co/rest/v1/T&SL?limit=9", {
   method: "GET",
   headers: {
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4Y2dwcHpzaWhxaGVsbWxseXJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NTg3NDgsImV4cCI6MjA0MTUzNDc0OH0.ZRk78YiB5wffWcrQYr-CRsh2yrb7ISHSGx4V7rQuO_8",
