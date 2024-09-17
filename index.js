65225b14395ffa2d40bd540d4366688f502956ec
Emma <empe0005@stud.kea.dk>
2024-09-17 11:36:55 +0200

    mere js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch("https://axcgppzsihqhelmllyrd.supabase.co/rest/v1/T&SL?limit=6", {
+fetch("https://axcgppzsihqhelmllyrd.supabase.co/rest/v1/T&SL", {
   method: "GET",
   headers: {
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4Y2dwcHpzaWhxaGVsbWxseXJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NTg3NDgsImV4cCI6MjA0MTUzNDc0OH0.ZRk78YiB5wffWcrQYr-CRsh2yrb7ISHSGx4V7rQuO_8",
@@ -8,6 +8,7 @@ fetch("https://axcgppzsihqhelmllyrd.supabase.co/rest/v1/T&SL?limit=6", {
   .then(showCategories);
 
 function showCategories(cats) {
+  console.log(cats);
   cats.forEach(showCategory);
 }
 
--- produkt.js
+++ produkt.js
@@ -1,1 +1,19 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("asset_id");
 
+window.addEventListener("load", getProduct);
+
+function getProduct() {
+  fetch("https://axcgppzsihqhelmllyrd.supabase.co/rest/v1/T&SL?limit=9", {
+    method: "GET",
+    headers: {
+      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4Y2dwcHpzaWhxaGVsbWxseXJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NTg3NDgsImV4cCI6MjA0MTUzNDc0OH0.ZRk78YiB5wffWcrQYr-CRsh2yrb7ISHSGx4V7rQuO_8",
+    },
+  })
+    .then((res) => res.json())
+    .then(visProdukt);
+}
+
+function visProdukt(product) {
+  document.querySelector("h2").textContent = product.produktnavn;
+}
