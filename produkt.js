a361e09cdab99a6d9ecc4ad69c8116e9d9662bbb
Emma <empe0005@stud.kea.dk>
2024-09-17 14:46:54 +0200

    js produktliste

--- produkt.js
+++ produkt.js
@@ -4,7 +4,7 @@ const id = urlParams.get("asset_id");
 window.addEventListener("load", getProduct);
 
 function getProduct() {
-  fetch("https://axcgppzsihqhelmllyrd.supabase.co/rest/v1/T&SL?limit=9", {
+  fetch(`https://axcgppzsihqhelmllyrd.supabase.co/rest/v1/T&SL?asset_id=eq.${id}`, {
     method: "GET",
     headers: {
       apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4Y2dwcHpzaWhxaGVsbWxseXJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NTg3NDgsImV4cCI6MjA0MTUzNDc0OH0.ZRk78YiB5wffWcrQYr-CRsh2yrb7ISHSGx4V7rQuO_8",
@@ -14,6 +14,6 @@ function getProduct() {
     .then(visProdukt);
 }
 
-function visProdukt(product) {
-  document.querySelector("h2").textContent = product.produktnavn;
+function visProdukt(data) {
+  console.log(data);
 }
--- produktliste.js
+++ produktliste.js
@@ -1,17 +1,40 @@
 const urlParams = new URLSearchParams(window.location.search);
 const category = urlParams.get("category");
+const encoded = encodeURIComponent(category);
+console.log(encoded);
 
-fetch("https://axcgppzsihqhelmllyrd.supabase.co/rest/v1/T&SL?limit=9", {
-  method: "GET",
+fetch(`https://axcgppzsihqhelmllyrd.supabase.co/rest/v1/T&SL?category=eq.${encoded}`, {
   headers: {
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4Y2dwcHpzaWhxaGVsbWxseXJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NTg3NDgsImV4cCI6MjA0MTUzNDc0OH0.ZRk78YiB5wffWcrQYr-CRsh2yrb7ISHSGx4V7rQuO_8",
   },
 })
   .then((res) => res.json())
-  .then(showProducts);
+  .then((data) => {
+    const uniqueProducts = new Set();
+    const filteredData = data.filter((item) => {
+      if (uniqueProducts.has(item.produktnavn)) {
+        return false;
+      } else {
+        uniqueProducts.add(item.produktnavn);
+        return true;
+      }
+    });
+    vis(filteredData);
+  });
 
-function showProducts(products) {
-  products.forEach(showProduct);
+function vis(data) {
+  console.table(data); // Dette viser dataene i konsollen
+
+  data.forEach((item) => {
+    const template = document.querySelector("template").content;
+    const copy = template.cloneNode(true);
+    copy.querySelector("h3").textContent = item.produktnavn;
+
+    document.querySelector("main").appendChild(copy);
+  });
+}
+
+/*   showProduct);
 }
 
 function showProduct(product) {
@@ -24,3 +47,16 @@ function showProduct(product) {
   copy.querySelector("a").href = `produkt.html?id=${product.id}`;
   document.querySelector(".produktliste-h1").appendChild(copy);
 }
+  .then((res) => res.json())
+  .then(showProducts);
+
+function showProducts(data) {
+  data.forEach((element) => {
+    const link = document.createElement("a");
+    link.href = `produkt.html?id=${element.asset_id}`;
+    link.textContent = element.produktnavn;
+    document.querySelector("main").appendChild(link);
+  });
+}
+
+*/
